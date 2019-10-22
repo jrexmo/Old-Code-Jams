6b6ddf53efe0e17909e6ece90ff57e3976ec94da
jrexmo <30879519+jrexmo@users.noreply.github.com>
2019-10-22 22:29:42 +0200

    Add files via upload
    Preliminary Round "Solution Forgone". Can make a version that works for N ~ 10^100 in python, but didn't get to it for C++.

---
+++ forgone.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+//#include <math>
+using namespace std;
+
+
+int solution(long num){
+  int digit = num%10;
+  int num1 = num;
+  vector<int> digits;
+
+
+  while(num1 != 0){
+    digits.push_back(digit);
+    num1 = (num1 - digit)/10;
+    digit = num1%10;
+  }
+int j;
+int A = 0;
+int B = 0;
+for(j = 0; j < digits.size() ; j++){
+  if(digits[j] == 4){
+    A = A + 2*pow(10, j);
+  }
+}
+  return A;
+}
+
+int main(){
+  int T;
+  cin >> T;
+  int i = 0;
+  long N;
+  vector<int> a;
+  for(i = 0; i < T; i++ ){
+    cin >> N;
+    a.push_back(N);
+  }
+  for(i = 0;i < T; i++){
+    cout << "Case #" << i + 1 << ": " << solution(a[i]) << " " << a[i] - solution(a[i]) << '\n';
+  }
+return 0;
+}
